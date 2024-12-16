188ab1ca6a8486f6f799fde6d21fc6415a1fe8a8
Anu9969 <sharmaanurag9969@gmail.com>
2024-12-16 10:50:49 +0530

    corrections done with routes

--- index.js
+++ index.js
@@ -2,24 +2,39 @@ const express = require('express');
 const port = 8000;
 const app = express();
 
-app.get('/', (req,res) => {
-    res.send('Feched Courses');
+
+app.post('/user/signup', (req,res) =>{
+    res.json({
+        message: "signup endpoint"
+    })
+})
+app.post('/user/login' ,function(req,re) {
+    res.json({
+        message: "login endpoint"
+    })
+})
+
+app.get('/course/purchase', (req,res) => {
+    //you expect user to pay
+    res.json({
+        message: "signup endpoint"
+    })
+})
+
+app.get('/user/purchases', (req,res) => {
+    res.json({
+        message: "user purchased courses"
+    })
 })
 
 app.get('/courses', (req,res) => {
-    res.send('purchased courses list')
-})
-app.post('/', (req,res)=>{
-    res.send('Course purchased as user submitted info')
-})
-
-app.put('/', (req,res) =>{
-
+    res.json({
+        message: "courses list"
+    })
 })
 
-app.delete('/',(req,res) =>{
 
-})
+
 
 
 app.listen(port, () => {
