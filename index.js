25fce1204fa2287dbf70d342fec1eb29e9eedf97
Anu9969 <sharmaanurag9969@gmail.com>
2024-12-16 10:42:31 +0530

    endpoint creation

--- index.js
+++ index.js
@@ -3,7 +3,22 @@ const port = 8000;
 const app = express();
 
 app.get('/', (req,res) => {
-    res.send('Hello World');
+    res.send('Feched Courses');
+})
+
+app.get('/courses', (req,res) => {
+    res.send('purchased courses list')
+})
+app.post('/', (req,res)=>{
+    res.send('Course purchased as user submitted info')
+})
+
+app.put('/', (req,res) =>{
+
+})
+
+app.delete('/',(req,res) =>{
+
 })
 
 
